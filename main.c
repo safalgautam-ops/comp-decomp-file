8476e4b4ebb73461b2b97733924b855a7ee79573
safalgautam-ops <gtmsafal09@gmail.com>
2025-03-06 11:40:20 +0545

    SECOND COMITTS

--- main.c
+++ main.c
@@ -2,24 +2,100 @@
 #include <stdlib.h>
 #include <string.h>
 
-//function to compress the file using RLE
+// Function to compress the file using RLE
 void compressFILE(FILE *inputFILE, FILE *outputFILE) {
     int currentChar, prevChar;
     int count = 1;
 
     prevChar = fgetc(inputFILE);
-    if(prevChar == EOF) {
+    if (prevChar == EOF) {
         return;
     }
 
-    while ((currentChar == fgetc(inputFILE))!=EOF) {
+    while ((currentChar = fgetc(inputFILE)) != EOF) {  // Fixed assignment
         if (currentChar == prevChar) {
-            count ++;
+            count++;
         } else {
-            fprintf(outputFILE, "%d%c", count,prevChar);
-            prevChar == currentChar;
+            fprintf(outputFILE, "%d%c", count, prevChar);
+            prevChar = currentChar;  // Fixed assignment
             count = 1;
         }
     }
-    fprintf(outputFILE,"%d%c",count, prevChar);
+    fprintf(outputFILE, "%d%c", count, prevChar);
+}
+
+// Function to decompress the file using RLE
+void decompressFile(FILE *inputFILE, FILE *outputFILE) {
+    int count;
+    char ch;
+    while (fscanf(inputFILE, "%d%c", &count, &ch) == 2) {  // Fixed fscanf check
+        for (int i = 0; i < count; i++) {
+            fputc(ch, outputFILE);
+        }
+    }
+}
+
+// Main function for input and output file handling
+int main() {
+    FILE *inputFILE, *outputFILE;
+    char inputFileName[100], outputFileName[100];
+    char ans;
+
+    printf("Do you want to compress or decompress?\n");
+    printf("Write c for compression and d for decompression \n");
+    scanf(" %c", &ans);  // Fixed space before %c
+
+    // Compression
+    if (ans == 'c') {
+        printf("Enter the input file name for compression: \n");
+        scanf("%s", inputFileName);
+        inputFILE = fopen(inputFileName, "r");
+        if (inputFILE == NULL) {
+            printf("Couldn't open the input file.\n");  // Fixed typo
+            return 1;
+        }
+
+        printf("Enter the output file name for compression: ");
+        scanf("%s", outputFileName);
+        outputFILE = fopen(outputFileName, "w");
+        if (outputFILE == NULL) {
+            printf("Could not open the output file.\n");
+            fclose(inputFILE);
+            return 1;
+        }
+
+        compressFILE(inputFILE, outputFILE);
+        printf("File compressed successfully!\n");
+        fclose(inputFILE);
+        fclose(outputFILE);
+    } else if (ans == 'd') {
+        // Decompression
+        printf("Enter the input file name for decompression: ");
+        scanf("%s", inputFileName);
+        inputFILE = fopen(inputFileName, "r");
+
+        if (inputFILE == NULL) {
+            printf("Could not open the input file.\n");
+            return 1;
+        }
+
+        printf("Enter the output file name for decompression: ");
+        scanf("%s", outputFileName);
+        outputFILE = fopen(outputFileName, "w");
+
+        if (outputFILE == NULL) {
+            printf("Could not open the output file.\n");
+            fclose(inputFILE);
+            return 1;
+        }
+
+        decompressFile(inputFILE, outputFILE);
+        printf("File decompressed successfully!\n");
+        fclose(inputFILE);
+        fclose(outputFILE);
+    } else {
+        printf("Input Error\n");
+    }
+
+    return 0;
 }
